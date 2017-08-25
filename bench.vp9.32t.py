f36bf1f5d91a2013fc9459864a1dae06ce581afc
nwgat <github@nwgat.net>
2017-08-25 02:21:05 +0200

    Update bench.vp9.32t.py

--- bench.vp9.32t.py
+++ bench.vp9.32t.py
@@ -5,7 +5,7 @@ import time
 import os
 import sys
 sys.stdout=open("output.vp9.txt","a")
-cmd = 'vpxenc.exe -w 3840 -h 2160 --row-mt=1 --tile-columns=6 --auto-alt-ref=1 --lag-in-frames=25 --aq-mode=0 --target-bitrate=20000 --cpu-used=1 --threads=16 -o test.webm TheFireEscape.raw.y4m'
+cmd = 'vpxenc.exe -w 3840 -h 2160 --row-mt=1 --tile-columns=6 --auto-alt-ref=1 --lag-in-frames=25 --aq-mode=0 --target-bitrate=20000 --cpu-used=2 --threads=16 -o test.webm TheFireEscape.raw.y4m'
 start_time = time.time()
 os.system(cmd)
 print("%s" % (time.time() - start_time))
